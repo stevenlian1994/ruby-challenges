40c95e627151aaf3c028197b690cc2f06f518d17
Steven Lian <stevenlian1994@gmail.com>
2017-11-06 11:27:23 +0000

    hashes and recursion todo

--- rubeque/one_unlike_other/one_unlike_other.md
+++ rubeque/one_unlike_other/one_unlike_other.md
@@ -9,3 +9,11 @@ min_by method which returns the lowest value
 Practiced:
 
 Learned to condense a bunch of methods on the same object into one line
+
+http://www.rubeque.com/problems/one-of-these-is-not-like-the-others
+
+Tags:
+arrays, classes
+
+Instructions:
+One of these is not like the others. Write a method that will return the one that doesn't match.
--- rubeque/one_unlike_other/one_unlike_other.rb
+++ rubeque/one_unlike_other/one_unlike_other.rb
@@ -22,3 +22,29 @@ puts nonmatching([1, 2, 3, 4.0, 5, 6])
 puts nonmatching(["ichi", "visitor q", 13, "assassins"])
 puts nonmatching([/2001/, "full metal jacket", /paths of glory/])
 puts nonmatching([true, false, nil, (1==1)])
+
+# OVER-RIDING PARENT CLASS METHOD: METHOD OVERRIDING
+# There will be situations where certain properties of the super-class should not be inherited by a particular subclass. 
+# METHOD OVERRIDING allows a subclass to provide a specific implementation of a method that is already provided by one of its superclasses.
+
+class Bird  
+  def preen  
+    puts "I am cleaning my feathers."  
+  end  
+  def fly  
+    puts "I am flying."  
+  end  
+end  
+  
+class Penguin < Bird  
+  def fly  
+    puts "Sorry. I'd rather swim."  
+  end  
+end  
+  
+p = Penguin.new  
+p.preen  #=> I am cleaning my feathers. Penguins inherited preen method normally, 
+p.fly  #=> Sorry I'd rather swim.  Fly method overriden
+
+#PENGUIN IS SUB-CLASS OF BIRD
+#TRUECLASS IS SUB-CLASS OF OBJECT
