fe5de748d75a70cd42d3982395a6c8d10623fc05
Steven Lian <stevenlian1994@gmail.com>
2017-11-23 09:36:37 +0000

    happy numbers

--- rubeque/happy_number/happy_number.rb
+++ rubeque/happy_number/happy_number.rb
@@ -16,18 +16,18 @@ end
 
 #OTHER INTERESTING SUBMISSIONS BY OTHER DEVS
 
-def happy_number?(n)
-  cache = []
-  x = n
+# def happy_number?(n)
+#   cache = []
+#   x = n
 
-  until cache.include?(x) or x == 1
-    cache << x
-    digits = x.to_s.chars.map(&:to_i)
-    x = digits.inject(0){|sum, digit| sum + digit**2}
-  end
+#   until cache.include?(x) or x == 1
+#     cache << x
+#     digits = x.to_s.chars.map(&:to_i)
+#     x = digits.inject(0){|sum, digit| sum + digit**2}
+#   end
 
-  x == 1
-end
+#   x == 1
+# end
 
 # def happy_number?(n)
 #   past = []
@@ -38,3 +38,10 @@ end
 #     past << n
 #   end
 # end
+
+# Happy numbers are positive integers that follow a particular formula: 
+# take each individual digit, square it, and then sum the squares to get a new number. 
+# Repeat with the new number and eventually, you might get to a number whose squared sum is 1. 
+# This is a happy number.
+# An unhappy number (or sad number) is one that loops endlessly. Write a function that determines 
+# if a number is happy or not.
